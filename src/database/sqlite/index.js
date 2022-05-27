52f68d5ffb0a838620c7a078425ec850b342abd0
Johnny Deymisson <jdeymisson@gmail.com>
2022-05-27 09:16:50 -0300

    finalizando a comunicação com o banco de dados

---
+++ src/database/sqlite/index.js
@@ -0,0 +1,13 @@
+const sqlite3 = require("sqlite3");
+const sqlite = require("sqlite");
+const path = require("path");
+
+function sqliteConnection() {
+  sqlite.open({
+    filename: path.resolve(__dirname, "../", "database.db"),
+    driver: sqlite3.Database
+  });
+};
+
+module.exports = sqliteConnection;
+
--- src/server.js
+++ src/server.js
@@ -1,13 +1,15 @@
 require("express-async-errors");
 const AppError = require("./utils/AppError");
-
 const express = require("express");
 const routes = require("./routes");
+const database = require("./database/sqlite");
 
 const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+database();
 
 app.use((error, request, response, next) => {
   if(error instanceof AppError) {
@@ -25,6 +27,6 @@ app.use((error, request, response, next) => {
   });
 });
 
-const PORT = 2222;
+const PORT = 2221;
 
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
