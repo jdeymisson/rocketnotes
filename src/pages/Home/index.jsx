502b6300364ea1c99232fb36fa526e18ec60eba7
Johnny Deymisson <jdeymisson@gmail.com>
2023-03-31 22:36:43 -0300

    distribuindo o grid na aplicação

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,30 @@
+import { Container, Brand, Menu, Search, Content, NewNotes } from './styles';
+import { Header } from '../../components/Header';
+
+export const Home = () => {
+    return(
+        <Container>
+            <Brand>
+                <h1>Rocketnotes</h1>
+            </Brand>
+
+            <Header />
+            
+            <Menu>
+
+            </Menu>
+
+            <Search>
+
+            </Search>
+
+            <Content>
+
+            </Content>
+
+            <NewNotes>
+
+            </NewNotes>
+        </Container>
+    )
+};
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+
+    display: grid;
+    grid-template-columns: 25rem auto;
+    grid-template-rows: 10.5rem 12.8rem auto 6.4rem;
+    grid-template-areas:
+    "brand header"
+    "menu search"
+    "menu content"
+    "newnote content";
+
+    background: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+`;
+export const Brand = styled.div`
+    grid-area: brand;
+    background: red;
+`;
+export const Menu = styled.ul`
+    grid-area: menu;
+    background: yellow;
+`;
+export const Search = styled.div`
+    grid-area: search;
+    background: pink;
+`;
+export const Content = styled.div`
+    grid-area: content;
+    background: #ccc;
+`;
+export const NewNotes = styled.button`
+    grid-area: newnote;
+    background: #777
+`;
